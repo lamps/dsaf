8721cbff9922f2a97347b4f0fcd0e05ecbe6c223
lamps <lamp94@netsgo.com>
2012-05-22 17:09:47 +0900

    test

---
+++ redmine_group_assignee/app/models/groups_assigned.rb
@@ -0,0 +1,9 @@
+class GroupsAssigned < ActiveRecord::Base
+  belongs_to :group
+  belongs_to :user
+   
+  def self.deleteAssignee(group_id,user_id)
+    delete_all(:group_id => group_id, :user_id => user_id )
+  end
+  
+end
